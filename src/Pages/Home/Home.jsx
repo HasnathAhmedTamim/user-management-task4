edfb0861d78e94841b631e07672f7293337c1734
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2024-07-28 22:24:43 +0600

    update

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,7 +4,7 @@
 const Home = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold">Welcome to the Home Page <br />Login First  </h1>
+      <h1 className="text-3xl font-bold">Welcome to the Home Page</h1>
     </div>
   );
 };
